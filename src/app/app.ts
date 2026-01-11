e13c0e59153d74ac0b2e4d987c24427e361b1e0a
Kevyn <kevyn.klein45@gmail.com>
2026-01-11 16:33:45 +0100

    add import

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { Header} from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
